0cbb871dc17c8c2de65feab23344312243ae97e4
brunobus <75343075+brunobus@users.noreply.github.com>
2020-12-30 22:15:30 +0100

    dodao init za Spring boot

---
+++ src/main/java/hr/fer/rassus/rassus_lti_grupa_16/RassusLtiGrupa16Application.java
@@ -0,0 +1,13 @@
+package hr.fer.rassus.rassus_lti_grupa_16;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RassusLtiGrupa16Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RassusLtiGrupa16Application.class, args);
+	}
+
+}
---
+++ src/test/java/hr/fer/rassus/rassus_lti_grupa_16/RassusLtiGrupa16ApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.fer.rassus.rassus_lti_grupa_16;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RassusLtiGrupa16ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
